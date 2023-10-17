5d91ca9dee0011f22e5b57474c0c73e3db42f5fe
Ross Brown <ross@crema.us>
2023-10-17 16:23:47 -0500

    removes copied modules

--- src/hunters_incoming/main.py
+++ src/hunters_incoming/main.py
@@ -1,9 +1,10 @@
 import os
+import json
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 
-from .kafka import send
 from .lib.models.alert import Alert
 
 kafka_bootstrap_servers: str = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
@@ -14,9 +15,28 @@ load_dotenv()
 topic: str = os.environ.get("TOPIC")
 
 
-# @app.on_event("startup")
-# async def startup_event():
-#     print("Starting up Hunters Incoming...")
+async def send(topic: str, msg: Alert):
+    print(f"Sending to Kafka: {topic} : {msg}")
+    try:
+        producer = AIOKafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+        await producer.start()
+
+        try:
+            await producer.send_and_wait(topic, kafka_serializer(msg))
+        finally:
+            await producer.stop()
+
+    except Exception as err:
+        print(f"Some Kafka error: {err}")
+
+
+def kafka_serializer(value):
+    return json.dumps(value).encode()
+
+
+@app.on_event("startup")
+async def startup_event():
+    print("Starting up Hunters Incoming...")
 
 
 @app.post("/")
--- src/lib/models/alert.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from pydantic import BaseModel
-
-
-class Alert(BaseModel):
-    timestamp: int | None = time.time()
-    description: str | None = None
-    severity: int | None = 1
